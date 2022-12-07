c4771881d0a650d1aa84bbf6364c726a2cd708bd
ShuyaLii <119496475+ShuyaLii@users.noreply.github.com>
2022-12-07 20:57:11 +0800

    Update csv_reader.py

--- csv_reader.py
+++ csv_reader.py
@@ -2,13 +2,13 @@ import csv
 import os
 import matplotlib.pyplot as plt
 
-def read_csv(fold):
+def get_epoch_data(fold):
     interval = 360
 
     fold_walk = os.walk(fold)
     for path, dir_list, file_list in fold_walk:
        for file in file_list:
-           if file != '.DS_Store':
+           if file.find('_data.csv') > -1:
                file_path = os.path.join(path, file)
 
                tg_fold = os.path.join(path, 'epoch_data')
@@ -76,6 +76,11 @@ def draw_chart(fold):
                 r.close()
 
 def draw_chart_multi(fold, tg_fold):
+
+    tg_fold = os.path.join(tg_fold, 'figures')
+    if not os.path.exists(tg_fold):
+        os.mkdir(tg_fold)
+
     dic = {'reward': 3,
            'att': 4,
            'throughput': 5,
@@ -85,12 +90,8 @@ def draw_chart_multi(fold, tg_fold):
     fold_walk = os.walk(fold)
     for path, dir_list, file_list in fold_walk:
         for file in file_list:
-            if file.find('epoch') > -1 and file.find('csv') > -1:
+            if file.find('epoch.csv') > -1:
                 file_path = os.path.join(path, file)
-
-                #tg_fold = os.path.join(path, 'epoch_figure')
-                #if not os.path.exists(tg_fold):
-                #   os.mkdir(tg_fold)
 
                 r = open(file_path, 'rt')
                 reader = csv.reader(r)
@@ -131,11 +132,72 @@ def draw_chart_multi(fold, tg_fold):
 
                 r.close()
 
+def get_final_result(fold, tg_fold):
+
+    tg_fold = os.path.join(tg_fold, 'number')
+    if not os.path.exists(tg_fold):
+        os.mkdir(tg_fold)
+
+    tg_file_path = os.path.join(tg_fold, 'result.csv')
+    w = open(tg_file_path, 'w', newline='')
+    writer = csv.writer(w)
+    writer.writerow(['data_set', 'epoch',
+                     'train_rwd', 'train_att', 'train_throughput', 'train_lay_delay',
+                     'test_rwd', 'test_att', 'test_throughput', 'test_lay_delay'])
+
+    data = ['' for i in range(10)]
+    train_flag = False
+    test_flag = False
+
+    dict = {'r': [2, 3, 4, 5],
+            'e': [6, 7, 8, 9]}
+
+    fold_walk = os.walk(fold)
+    for path, dir_list, file_list in fold_walk:
+        for file in file_list:
+            if file.find('_epoch.csv') > -1:
+
+                file_path = os.path.join(path, file)
+                r = open(file_path, 'r')
+                reader = csv.reader(r)
+
+                data_half = []
+                for idx, row in enumerate(reader):
+                    data_half = row
+
+                _i = 3
+                for i in dict[file[1]]:
+                    data[i] = data_half[_i]
+                    _i += 1
+
+                if not train_flag and file[1] == 'r':
+                    train_flag = True
+
+                    str_start = path.find('data') + (len(fold) - 2) + 1
+                    str_end = path.find('1h') + 2
+                    data_set = path[str_start: str_end]
+                    data[0] = data_set
+
+                if not test_flag and file[1] == 'e':
+                    test_flag = True
+                    data[1] = data_half[1]
+
+                if train_flag and test_flag:
+                    writer.writerow(data)
+                    train_flag = False
+                    test_flag = False
+
+                r.close()
+
+    w.close()
+
+
 if __name__ == '__main__':
 
    fold_path = './data_1x1_14'
-   tg_fold_path = './data_1x1_14/figures'
+   tg_fold_path = './data_1x1_14'
 
-   #read_csv(fold_path)
+   #get_epoch_data(fold_path)
    #draw_chart(fold_path)
-   draw_chart_multi(fold_path, tg_fold_path)
+   #draw_chart_multi(fold_path, tg_fold_path)
+   get_final_result(fold_path, tg_fold_path)
